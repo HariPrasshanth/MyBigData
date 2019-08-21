d42cd5c13f2de512e7ce073f8cf6b2f4a2830ac6
HariPrasshanth <48590674+HariPrasshanth@users.noreply.github.com>
2019-08-21 13:29:49 +0530

    Add files via upload

---
+++ TotalSpentByCustomer.scala
@@ -0,0 +1,39 @@
+package com.spark.learn
+
+import org.apache.spark._
+import org.apache.log4j._
+
+/** Compute the total amount spent per customer in some fake e-commerce data. */
+object TotalSpentByCustomer {
+  
+  /** Convert input data to (customerID, amountSpent) tuples */
+  def extractCustomerPricePairs(line: String) = {
+    val fields = line.split(",")
+    (fields(0).toInt, fields(2).toFloat)
+  }
+ 
+  /** Our main function where the action happens */
+  def main(args: Array[String]) {
+   
+    // Set the log level to only print errors
+    Logger.getLogger("org").setLevel(Level.ERROR)
+    
+     // Create a SparkContext using every core of the local machine
+    val sc = new SparkContext("local[*]", "TotalSpentByCustomer")   
+    
+    val input = sc.textFile("C:/Users/pc/workspace/customer-orders.csv")
+   println(input.partitions.length)
+    val mappedInput = input.map(extractCustomerPricePairs)
+    
+    val totalByCustomer = mappedInput.reduceByKey( (x,y) => x + y )
+       println(totalByCustomer.partitions.length)
+
+    val results = totalByCustomer.sortByKey().collect
+    
+    // Print the results.
+    results.foreach(println)
+    
+  }
+  
+}
+
